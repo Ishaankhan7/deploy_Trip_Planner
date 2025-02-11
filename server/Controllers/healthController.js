255199420557c90539149670b9a13bf63f7d5eea
ishaan khan <ishaank704@gmail.com>
2025-02-11 20:19:40 +0530

    Fixed Model error

--- server/Controllers/healthController.js
+++ server/Controllers/healthController.js
@@ -1,5 +1,5 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-const HealthModel = require('../models/healthModel');
+const HealthModel = require('../Models/healthModel');
 const jwt = require('jsonwebtoken');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
